9bea5765f994ffe31b5d53f6f3e04f59d7ae5a90
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-07-12 16:07:05 +0000

    Remove code meant for debugging

--- tts_project/settings/dev.py
+++ tts_project/settings/dev.py
@@ -26,7 +26,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 # Email settings for local development (console backend)
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # DEFAULT_FROM_EMAIL = "webmaster@localhost"  # A dummy email for dev
-DEFAULT_FROM_EMAIL = "jay09peterson@gmail.com"
+DEFAULT_FROM_EMAIL = "webmaster@localhost"
 
 # Optionally, for Debug Toolbar (if you install it later)
 # INSTALLED_APPS += ['debug_toolbar']
@@ -59,12 +59,4 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
-# tts_project/settings/dev.py
-# ...
-print(
-    f"DEBUG: GOOGLE_CLIENT_ID loaded: '{config('GOOGLE_CLIENT_ID', default='NOT_SET')}'"
-)
-print(
-    f"DEBUG: GITHUB_CLIENT_ID loaded: '{config('GITHUB_CLIENT_ID', default='NOT_SET')}'"
-)
-# ...
+

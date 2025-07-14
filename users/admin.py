5e410207797814e22466a5d7f4da586ea8a774d1
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-07-14 12:02:15 +0000

    Update users/admin.py file

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Define which fields to show in the list view of the admin
+    list_display = (
+        "email",
+        "username",
+        "is_staff",
+        "is_active",
+    )
+    # Define which fields can be used for searching
+    search_fields = (
+        "email",
+        "username",
+    )
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

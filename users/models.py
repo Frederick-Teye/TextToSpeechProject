9d9fa9eafac73c17b98f465e01df5b1e9c1e984c
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-07-14 11:43:23 +0000

    Creat CustomUser class in users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom User Model.
+    We are extending the default user to allow for future customization.
+    The email field will be used for login instead of the username.
+    """
+
+    # We are overriding the default email field to make it unique,
+    # as this will be our main identifier for users.
+    email = models.EmailField(unique=True, verbose_name="email address")
+
+    # Tell Django that the `email` field is the "username" for logging in.
+    USERNAME_FIELD = "email"
+
+    # The `createsuperuser` command will ask for these fields.
+    # We still need a username for Django's internals, but it's not for login.
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email

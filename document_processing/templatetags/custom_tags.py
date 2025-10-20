788ca82c766d0225cab31c23e9c48a7bc0c0c6a0
Frederick-Teye <144501703+Frederick-Teye@users.noreply.github.com>
2025-10-20 15:52:54 +0000

    Change and markdown templatetag into md_to_html templatetag
    Upgrade md_to_html templatetag to format mardown texts better.

--- document_processing/templatetags/custom_tags.py
+++ document_processing/templatetags/custom_tags.py
@@ -1,11 +1,22 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.safestring import mark_safe
 import markdown as md
 
 register = template.Library()
 
 
-@register.filter()
+@register.filter(name="md_to_html")
 @stringfilter
-def markdown(value):
-    return md.markdown(value, extensions=["markdown.extensions.fenced_code"])
+def md_to_html(value):
+    html = md.markdown(
+        value,
+        extensions=[
+            "fenced_code",
+            "codehilite",  # syntax highlighting (requires Pygments)
+            "tables",  # table support
+            "toc",  # optional: table of contents
+        ],
+        output_format="html5",
+    )
+    return mark_safe(html)
